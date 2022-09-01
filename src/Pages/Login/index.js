7b9ac67717bd6962dd2beb68f7fa47dc3963238d
luiz <luizmarcelolm@hotmail.com>
2022-08-31 22:26:37 -0300

    config inputs

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,18 +1,22 @@
 import react from 'react'
 import {Container, Form} from'./styles'
+import Input from '../../Components/Input/index'
 
 const Login = () => {
     return (
        <Container>
           <Form>
+            <input/>
+            <input/>
             <h1> Faça seu Login </h1>
+          
             <div>
                 <p>Não possui conta?</p>
                 <a>Cadastrar</a>
             </div>
           </Form>
        </Container>
-    );
+    )
 }
 
 export default Login;
