62e56353128ee664bffdf5c0452926b9ca105c00
luiz <luizmarcelolm@hotmail.com>
2022-09-01 20:19:50 -0300

    alteração app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+   <div>
+    <p>TELA DE LOGIN</p>
+   </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './Pages/Login/index'
-import GlobalStyle from './styles/global'
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle/>
-    <Login />
+    <App />
   </React.StrictMode>
 );
-
 
 
