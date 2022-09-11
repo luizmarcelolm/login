d672014e49f4595b676e78cb3550fe911fd32fff
luiz <luizmarcelolm@hotmail.com>
2022-09-11 17:16:24 -0300

    teste31

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -37,7 +37,7 @@ export const Login = () => {
                     <form className="login-form">
 
                         { /* título da tela de login  */ }
-                        <span className="login-form-title"> 30 SEJA BEM VINDO!</span>
+                        <span className="login-form-title"> 31 SEJA BEM VINDO!</span>
                         
                         { /* criação do input do email  */ }
                         <div className="wrap-input">
