436f91ae63108cf044ccfa3697ddd569c7fd6967
luiz <luizmarcelolm@hotmail.com>
2022-09-11 16:42:24 -0300

    teste27

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -32,12 +32,12 @@ export const AuthProvider = ({ children }) => {
         return;
       } else {
         //alert("Email ou senha incorretos");
-        return;// Navigate("./Login");       
+        return  Navigate("./Login");       
       }
 
     } else {
       //alert("Usuário não cadastrado");
-      return;// Navigate("./Login");
+      return Navigate("./Login");
     }
   };
 
